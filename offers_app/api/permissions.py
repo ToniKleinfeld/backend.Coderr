5061033deeb7581dff13028d2c677baba4a525a6
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-06-17 22:37:22 +0200

    feat(api): add permission for offers get - no Permission needed detail - only auth users Post - only business user patch/delte - only creator of post

---
+++ offers_app/api/permissions.py
@@ -0,0 +1,37 @@
+from rest_framework import permissions
+
+
+class OffersPermission(permissions.IsAuthenticated):
+    """
+    Custom Permission for Offers:
+    - LIST: No authentication required
+    - POST: User must be authenticated and have type 'business'
+    - RETRIEVE/PATCH/DELETE: User must be authenticated
+    - PATCH/DELETE: User must be the offer.user of the offer and have type 'business'
+    """
+
+    def has_permission(self, request, view):
+        """
+        Checks if the user is generally authorized
+        """
+        if view.action == 'list':
+            return True
+
+        if not super().has_permission(request, view):
+            return False
+        
+        if request.method == 'POST':
+            if not hasattr(request.user, 'type') or request.user.type != 'business':
+                return False
+        
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Checks if the user is authorized to access the specific review object
+        """
+
+        if request.method in ['PATCH', 'DELETE']:
+            return obj.user == request.user
+        
+        return True
