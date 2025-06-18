f39659f264a4940c004792edc81b5e1647542b61
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-06-18 14:55:29 +0200

    test: add Post tests

--- offers_app/tests/test_offers.py
+++ offers_app/tests/test_offers.py
@@ -43,6 +43,51 @@ class OfferTestSetup(APITestCase):
             type="customer",
         )
 
+        self.post_data = {
+            "title": "New Offer",
+            "description": "This is a new offer.",
+            "details": [
+                {
+                    "title": "Basic Package",
+                    "revisions": 2,
+                    "delivery_time_in_days": 3,
+                    "price": 100.00,
+                    "offer_type": "basic",
+                    "features": ["Feature 1", "Feature 2"],
+                },
+                {
+                    "title": "Standard Package",
+                    "revisions": 3,
+                    "delivery_time_in_days": 5,
+                    "price": 200.00,
+                    "offer_type": "standard",
+                    "features": ["Feature 1", "Feature 2", "Feature 3"],
+                },
+                {
+                    "title": "Premium Package",
+                    "revisions": 5,
+                    "delivery_time_in_days": 7,
+                    "price": 300.00,
+                    "offer_type": "premium",
+                    "features": ["Feature 1", "Feature 2", "Feature 3", "Feature 4"],
+                },
+            ],
+        }
+
+        self.patch_data = {
+            "title": "Updated Offer",
+            "details": [
+                {
+                    "title": "Basic Package",
+                    "revisions": 3,
+                    "delivery_time_in_days": 4,
+                    "price": 150.00,
+                    "offer_type": "basic",
+                    "features": ["Feature 1", "Feature 2", "Feature 3"],
+                }
+            ],
+        }
+
         self.types = ["business", "customer"]
 
         Offer.objects.create(
@@ -122,21 +167,30 @@ class OfferListTestCase(OfferTestSetup):
 
         response = self.client.get(reverse("offers:offers-list"))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 4) # 3 Pagination + 1 results with offers
-        self.assertEqual(len(response.data["results"]), 1) # Only one offer created in setUp
-        self.assertEqual(response.data["results"][0]["details"][0]["url"], "/offerdetails/1/")
+        self.assertEqual(len(response.data), 4)  # 3 Pagination + 1 results with offers
+        self.assertEqual(
+            len(response.data["results"]), 1
+        )  # Only one offer created in setUp
+        self.assertEqual(
+            response.data["results"][0]["details"][0]["url"], "/offerdetails/1/"
+        )
         self.assertEqual(response.data["count"], 1)
-
 
     def test_filtered_offer_list_creator_id(self):
         """
         Test if the offer list can be filtered by creator_id.
         """
-        response = self.client.get(reverse("offers:offers-list"), {"creator_id": self.first_business_user.id})
+        response = self.client.get(
+            reverse("offers:offers-list"), {"creator_id": self.first_business_user.id}
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["count"], 1)  # Only one offer created by the first business user
+        self.assertEqual(
+            response.data["count"], 1
+        )  # Only one offer created by the first business user
         self.assertEqual(len(response.data["results"]), 1)
-        self.assertEqual(response.data["results"][0]["user"], self.first_business_user.id)
+        self.assertEqual(
+            response.data["results"][0]["user"], self.first_business_user.id
+        )
 
     def test_filtered_offer_list_min_price(self):
         """
@@ -150,7 +204,9 @@ class OfferListTestCase(OfferTestSetup):
         """
         Test if the offer list can be filtered by min_delivery_time.
         """
-        response = self.client.get(reverse("offers:offers-list"), {"min_delivery_time": 2})
+        response = self.client.get(
+            reverse("offers:offers-list"), {"min_delivery_time": 2}
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["count"], 1)
 
@@ -158,12 +214,13 @@ class OfferListTestCase(OfferTestSetup):
         """
         Test if the offer list can be filtered by search query.
         """
-        response = self.client.get(reverse("offers:offers-list"), {"search": "Test Offer 1"})
+        response = self.client.get(
+            reverse("offers:offers-list"), {"search": "Test Offer 1"}
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["count"], 1)
         self.assertEqual(len(response.data["results"]), 1)
         self.assertEqual(response.data["results"][0]["title"], "Test Offer 1")
-
 
 
 class OfferDetailTestCase(OfferTestSetup):
@@ -186,11 +243,11 @@ class OfferDetailTestCase(OfferTestSetup):
         url = response.data["details"][0]["url"]
         self.assertTrue(
             re.match(r"^http://.+/api/offerdetails/\d+/?$", url),
-            f"URL {url} stimmt nicht mit dem erwarteten Muster überein"
+            f"URL {url} stimmt nicht mit dem erwarteten Muster überein",
         )
 
         parsed = urlparse(url)
-        self.assertEqual(parsed.path, f"/api/offerdetails/{expected_id.id}/") 
+        self.assertEqual(parsed.path, f"/api/offerdetails/{expected_id.id}/")
 
     def test_offer_detail_without_auth(self):
         """
@@ -208,5 +265,88 @@ class OfferDetailTestCase(OfferTestSetup):
         response = self.client.get(reverse("offers:offers-detail", args=[9999]))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-        
-        
+
+class OfferCreateTestCase(OfferTestSetup):
+
+    def setUp(self):
+        super().setUp()
+        self.post_data_less_details = {
+            "title": "New Offer",
+            "description": "This is a new offer.",
+            "details": [
+                {
+                    "title": "Basic Package",
+                    "revisions": 2,
+                    "delivery_time_in_days": 3,
+                    "price": 100.00,
+                    "offer_type": "basic",
+                    "features": ["Feature 1", "Feature 2"],
+                },
+                {
+                    "title": "Standard Package",
+                    "revisions": 3,
+                    "delivery_time_in_days": 5,
+                    "price": 200.00,
+                    "offer_type": "standard",
+                    "features": ["Feature 1", "Feature 2", "Feature 3"],
+                },
+            ],
+        }
+
+    def test_create_offer_with_details(self):
+        """
+        Test if an offer can be created with details.
+        """
+        self.authenticate_user(user_type="business", custom_user_number="1")
+
+        response = self.client.post(
+            reverse("offers:offers-list"), self.post_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["title"], self.post_data["title"])
+        self.assertEqual(len(response.data["details"]), 3)
+
+    def test_create_offer_without_details(self):
+        """
+        Test if an offer cannot be created without details.
+        """
+        self.authenticate_user(user_type="business", custom_user_number="1")
+
+        data = {
+            "title": "New Offer Without Details",
+            "description": "This offer has no details.",
+        }
+
+        response = self.client.post(reverse("offers:offers-list"), data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_offer_with_less_details(self):
+        """
+        Test if an offer cannot be created with less than 3 details.
+        """
+        self.authenticate_user(user_type="business", custom_user_number="1")
+
+        response = self.client.post(
+            reverse("offers:offers-list"), self.post_data_less_details, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_without_authentication(self):
+        """
+        Test if an offer cannot be created without authentication.
+        """
+        response = self.client.post(
+            reverse("offers:offers-list"), self.post_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_offer_user_not_business(self):
+        """
+        Test if an offer cannot be created by a user who is not a business.
+        """
+        self.authenticate_user(user_type="customer", custom_user_number="1")
+
+        response = self.client.post(
+            reverse("offers:offers-list"), self.post_data, format="json"
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
