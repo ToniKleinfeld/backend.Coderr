9adbd5a6b4c0bb3a3b756b30618d9c7a08376e35
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-08-02 12:08:43 +0200

    style(profiles_urls): standardize string quotes in URL patterns

--- profiles_app/api/profiles_urls.py
+++ profiles_app/api/profiles_urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import BusinessProfileListView, CustomerProfileListView
 
-app_name = 'profiles'
+app_name = "profiles"
 
 urlpatterns = [
-    path('business/', BusinessProfileListView.as_view(), name='business-profiles-list'),
-    path('customer/', CustomerProfileListView.as_view(), name='customer-profiles-list'),
+    path("business/", BusinessProfileListView.as_view(), name="business-profiles-list"),
+    path("customer/", CustomerProfileListView.as_view(), name="customer-profiles-list"),
 ]
