d0c26c7e216986a621bac5930b8628b41ba6165c
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-09-03 13:17:40 +0200

    feat(settings): add STATIC and MEDIA file configurations

--- core/settings.py
+++ core/settings.py
@@ -153,6 +153,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
